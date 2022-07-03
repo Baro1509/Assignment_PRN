7591a2db928691b615f64f70450082821e400a48
tranguyenn <nbtran.work@gmail.com>
2022-07-03 16:12:43 +0700

    order and order details

--- Ass02Solution/DataAccess/OrderDetailDAO.cs
+++ Ass02Solution/DataAccess/OrderDetailDAO.cs
@@ -3,8 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObject.EntityModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.ComponentModel.DataAnnotations;
 
 namespace DataAccess {
-    internal class OrderDetailDAO {
+    public class OrderDetailDAO {
+        private static OrderDetailDAO instance = null;
+        private static readonly object instanceLock = new object();
+        public static OrderDetailDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new OrderDetailDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+        public OrderDetailDAO() { }
+        public List<OrderDetail> GetOrdersDetailsByOID(int OrderID)
+        {
+            List<OrderDetail> orderDetails;
+            try
+            {
+                Ass02Context context = new Ass02Context();
+                orderDetails = context.OrderDetails.Where(OrderDetail=> OrderDetail.OrderId.Equals(OrderID)).ToList();
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return orderDetails;
+        }
     }
 }
--- Ass02Solution/DataAccess/Repository/OrderRepository.cs
+++ Ass02Solution/DataAccess/Repository/OrderRepository.cs
@@ -3,8 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using BusinessObject.EntityModels;
 namespace DataAccess.Repository {
-    internal class OrderRepository {
+    public class OrderRepository : IOrderRepository
+    {
+        public List<Order>? GetOrders() =>  OrderDAO.Instance.GetOrders();
+        public List<Order>? GetOrdersBydate(DateTime start, DateTime end) => OrderDAO.Instance.GetOrdersBydate(start, end);
+        public List<Order>? GetOrdersMember(int memberID) => OrderDAO.Instance.GetOrdersMember(memberID);
+        public List<Order>? GetOrdersBydateMember(DateTime start, DateTime end, int memberID) => OrderDAO.Instance.GetOrdersBydateMember(start, end,memberID);
     }
 }
